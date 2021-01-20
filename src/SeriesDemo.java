ea808be62a8c6f96788f08794b08cda4c3dfd758
Harish Manana <harish.manana@stackroute.in>
2021-01-20 07:50:02 +0530

    Second time changes

--- src/SeriesDemo.java
+++ src/SeriesDemo.java
@@ -2,7 +2,7 @@
 public class SeriesDemo {
 	public static void main(String[] args) {
 		int no = 30;
-		
+		System.out.println("This is Series Demo ...");
 		System.out.println("for loop processing ...");
 		for(no = 1; no <= 10; no++) {
 			System.out.println("No - " + no);
